73af11471f65534dac3e17c80f61921355fa140e
Nacho123122 <juanignaciojimenezcondori122@gmail.com>
2024-01-18 01:38:32 -0400

    lafelafe

---
+++ problema-3/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Listanotas from './components/ListaNotas';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Listanotas />
+      
+    </div>
+  );
+}
+
+export default App;
